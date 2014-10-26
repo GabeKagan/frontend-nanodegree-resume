bc51fa9ba9c4dbd4be70129e627f7c7468fdc7ad
GabeKagan <gabekagan@live.com>
2014-10-26 13:55:35 -0400

    Bio and schools encapsulated

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,42 +5,56 @@ var bio = {
     "role": "Placeholder role",
     "age": 22,
     "contact": "Contact placeholder",
+
+    "contacts": {
+        "phone": "555-555-5555",
+        "email": "gabekagan@live.com",
+        "github": "GabeKagan",
+        "twitter": "N/A",
+        "location": "Westford, MA"
+    },
+
     "picture": "images/197x148.gif",
     "message": "Doopy doopy doop",
-    "skills": [
-        "Placeholder","More placeholder","Are you done yet?", "Guess not.", 
-        "This one shouldn't even show."
-        ] //Turn this into an array at some point
+    "skills": ["Placeholder","More placeholder","Are you done yet?", "Guess not.", 
+        "This one shouldn't even show."]
 };
 
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-var formattedContact = HTMLcontactGeneric.replace("%data%",bio.contact);
+var formattedContact = HTMLcontactGeneric.replace("%data%",bio.contact); //Remove? Rebuild?
 var formattedImg = HTMLbioPic.replace("%data%",bio.picture);
 var formattedMessage = HTMLWelcomeMsg.replace("%data%",bio.message);
 
-//Detailed contacts.
-var contacts = {
-    "phone": "555-555-5555",
-    "email": "gabekagan@live.com",
-    "github": "GabeKagan",
-    "twitter": "N/A",
-    "location": "Westford, MA"
-};
-
 //Add the more detailed contacts!
-var formattedMobile = HTMLmobile.replace("%data%",contacts.phone);
-var formattedEmail = HTMLemail.replace("%data%",contacts.email);
-var formattedGithub = HTMLgithub.replace("%data%",contacts.github);
-var formattedTwitter = HTMLtwitter.replace("%data%",contacts.twitter);
-var formattedLocation = HTMLlocation.replace("%data%",contacts.location);
+var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.phone);
+var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
+var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
 
-//Make this look better.
+//Next encapsulation target.
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 $("#header").append(formattedImg);
 $("#header").append(formattedMessage);
 $("#header").append(HTMLskillsStart);
+
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedLocation);
+
+//Append a skill list if we have one.
+//Refactor out the boolean compare. ".replace" isn't changing the original.
+if(bio.skills.length > 0) {
+    for( i = 0; i < bio.skills.length; i++) {
+        if (i === 4) { break }  //Don't append more than 4 items.
+        var formattedSkills = HTMLskills.replace(("%data%" || bio.skills[i-1]),bio.skills[i]); 
+        $("#header").append(formattedSkills);
+    } 
+}
 
 //Long list of jobs, mostly fake.
 var jobList = {
@@ -74,7 +88,6 @@ var jobList = {
         "description":"2This is also a placeholder."
     },
 
-    //Work bloc needs special code. Encapsulate this eventually.
     display: function() {
         for (var job in jobList) {
 
@@ -94,28 +107,36 @@ var jobList = {
     }
 }
 
-//Education
+//Education. May need reformatting.
 var school = {
     "name":"University of Rochester",
     "degree":"Bachelor of Arts",
     "dates":"Fall 2009 -> Summer 2013",
     "location":"Rochester, New York, United States of America",
     "major":"History",
+
+    display: function(){
+
+        $("#education").append(HTMLschoolStart);
+        var formattedSchoolName = HTMLschoolName.replace("%data%",school.name);
+        $(".education-entry").append(formattedSchoolName);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",school.degree);
+        $(".education-entry").append(formattedSchoolDegree);
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%",school.dates);
+        $(".education-entry").append(formattedSchoolDates);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",school.location);
+        $(".education-entry").append(formattedSchoolLocation);
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%",school.major);
+        $(".education-entry").append(formattedSchoolMajor);
+    }
 };
 
-var formattedSchoolName = HTMLschoolName.replace("%data%",school.name);
-var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",school.degree);
-var formattedSchoolDates = HTMLschoolDates.replace("%data%",school.dates);
-var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",school.location);
-var formattedSchoolMajor = HTMLschoolMajor.replace("%data%",school.major);
-
-//Needs to be coded properly? This parenthesis formatting is weird.
+//Note to self: This uses an anonymous function.
 $(document).click(function(loc) { 
     var x = loc.pageX;
     var y = loc.pageY;
     logClicks(x,y);
 });
-
 
 //Projects.
 var projects = {
@@ -160,28 +181,11 @@ var projects = {
     }
     
 }
+
+//Add all this data to the page. Change where it's appended?
 projects.display();
 jobList.display();
-
-//Add all this data to the page. Change where it's appended. 
-
-//Append a skill list if we have one.
-//For each instance, replace either the placeholder %data%, or the last thing we had.
-if(bio.skills.length > 0) {
-    for( i = 0; i < bio.skills.length; i++) {
-        if (i === 4) { break }  //Don't append more than 4 items.
-        var formattedSkills = HTMLskills.replace(("%data%" || bio.skills[i-1]),bio.skills[i]); 
-        $("#header").append(formattedSkills);
-    } 
-}
-
-//Contacts
-$("#topContacts").append(formattedMobile);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedGithub);
-$("#topContacts").append(formattedTwitter);
-$("#topContacts").append(formattedLocation);
-
+school.display();
 
 
 //Rewrite to use any list that has location data later
@@ -203,12 +207,6 @@ function inName(iName) {
     return inNameResult;
 }
 
-$("#education").append(HTMLschoolStart);
-$(".education-entry").append(formattedSchoolName);
-$(".education-entry").append(formattedSchoolDegree);
-$(".education-entry").append(formattedSchoolDates);
-$(".education-entry").append(formattedSchoolLocation);
-$(".education-entry").append(formattedSchoolMajor);
 
 
 $("#letsConnect").append(formattedContact); //Contact info goes at the bottom!
